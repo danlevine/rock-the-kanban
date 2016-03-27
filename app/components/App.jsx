d76c9cc685c33af84226958b3876cac30f27dbf0
Daniel Levine <dlevine210@gmail.com>
2016-03-27 01:27:50 -0500

    Break up App into Note and Notes components

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,8 @@
 import uuid from 'node-uuid';
 import React from 'react';
 
+import Notes from './Notes.jsx';
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -39,9 +41,7 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <ul>{notes.map(note =>
-          <li key={note.id}>{note.task}</li>
-        )}</ul>
+        <Notes notes={notes} />
       </div>
     );
 
